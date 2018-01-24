5d86eb2fdb05c1e83c87627dc734a11930004128
mohan <mohankukreja1@gmail.com>
2018-01-24 23:37:07 +0530

    clientid added
    Signed-off-by: mohan <mohankukreja1@gmail.com>

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports={
+    google:{
+        clientID:'723614240477-l430l4l29nuk9fueeo2i4durlu4oco6l.apps.googleusercontent.com',
+        clientSecret:'lJeej3s_IzOcTUMrNhx90T5y'
+    }
+}
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,9 +1,11 @@
 const passport=require('passport');
 const GoogleStratergy=require('passport-google-oauth20');
+const keys=require('./keys')
 
 passport.use(
     new GoogleStratergy({
-
+        clientId:keys.google.clientID,
+        clientSecret:keys.google.clientSecret
     }),()=>{
 
     }

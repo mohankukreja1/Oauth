c38238cd710203f389ef3424255227e9af4bf459
mohan <mohankukreja1@gmail.com>
2018-01-26 18:15:13 +0530

    db syncronyised
    Signed-off-by: mohan <mohankukreja1@gmail.com>

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport=require('passport');
 const GoogleStratergy=require('passport-google-oauth20');
 const keys=require('./keys')
+const user=require('../db/models').models.User
 
 passport.use(
     new GoogleStratergy({
@@ -8,8 +9,18 @@ passport.use(
         clientID:keys.google.clientID,
         clientSecret:keys.google.clientSecret
     },(accessToken,refreshtoken,profile,done)=>{
-        console.log('profile')
-    })
+        console.log(profile);
+        user.create({
+            name:profile.displayName,
+            ID:profile.id
+
+        })
+            .then((result)=>{
+                console.log(result);
+            }).catch((err)=>{
+                console.log(err);
+        })
+
 )
 
 
--- db/models.js
+++ db/models.js
@@ -3,8 +3,34 @@ const DB=require('../config1.json').DB
 
 let user=null;
 
-MongoClient.connect(DB.URI, function (err, db) {
+MongoClient.connect(DB.URI, function (err, database) {
     if (err) throw  err;
+    const userdb=database.db('tododb')
 
-    user = db.collection('user')
+    user = userdb.collection('user')
 })
+
+const User={
+    create: function (user1) {
+        return new Promise(function (resolve, reject) {
+            user.insertOne(user1, function (err, result) {
+                if (err) return reject(err)
+
+                return resolve(result)
+            })
+        })
+    },
+    findAll: function (whereArgs) {
+        return new Promise(function (resolve, reject) {
+            user.find(whereArgs).toArray(function (err, result) {
+                if (err) return reject(err)
+
+                return resolve(result)
+            })
+        })
+    }
+}
+
+exports.models = {
+    User
+}
